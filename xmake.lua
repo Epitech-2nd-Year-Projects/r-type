a2e1986692974b0c7f2531c2be4d7d066cab4d04
Yanis897349 <146654215+Yanis897349@users.noreply.github.com>
2025-11-18 13:21:49 +0000

    ci(r-type): mirroring

--- xmake.lua
+++ xmake.lua
@@ -8,7 +8,7 @@ set_warnings("all")
 set_policy("package.requires_lock", true)
 
 if is_plat("windows") then
-	set_toolset("ar", "llvm-lib")
+	set_toolset("ar", "llvm-ar")
 end
 
 includes("engine", "server", "client", "protocol", "game_logic")
