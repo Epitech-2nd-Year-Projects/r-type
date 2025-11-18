6594c8945e24e7bfe5fdfacd6e8aa3df880285cb
Yanis897349 <146654215+Yanis897349@users.noreply.github.com>
2025-11-18 12:52:09 +0000

    ci(r-type): llvm toolchain installation is painful

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-set -e
+set -euo pipefail
+
+if [[ "${OS:-}" == "Windows_NT" ]]; then
+  xmake f -p windows -a x64 -m release --toolchain=clang
+fi
+
 xmake
--- scripts/format-check.sh
+++ scripts/format-check.sh
@@ -7,4 +7,9 @@ fi
 
 echo "clang-format available at: $(command -v clang-format)"
 clang-format --version
+
+if [[ "${OS:-}" == "Windows_NT" ]]; then
+  xmake f -p windows -a x64 -m release --toolchain=clang
+fi
+
 xmake format --dry-run
