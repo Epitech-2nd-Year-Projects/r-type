226afed74bf5c8202e7d27c6459507857e26ea5d
Yanis897349 <146654215+Yanis897349@users.noreply.github.com>
2025-11-19 15:38:53 +0000

    feat(engine): define public API and module boundaries

--- engine/xmake.lua
+++ engine/xmake.lua
@@ -2,6 +2,6 @@ add_rules("mode.debug", "mode.release")
 
 target("engine")
 set_kind("static")
-set_default(false)
-add_files("src/*.cpp")
-
+add_files("src/**/*.cpp")
+add_includedirs("include")
+add_headerfiles("include/engine/*.hpp")
