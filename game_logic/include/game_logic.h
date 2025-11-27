7bfbc116226d0d9e9c66ab6511351d58d6b75b20
Enzogall <enzo.gallini@epitech.eu>
2025-11-27 11:55:44 +0100

    feat (game_logic): Setup game_logic include folder

---
+++ game_logic/include/game_logic.h
@@ -0,0 +1,9 @@
+#ifndef GAME_LOGIC_GAME_LOGIC_H_
+#define GAME_LOGIC_GAME_LOGIC_H_
+
+#include <game_logic/components.h>
+#include <game_logic/entities.h>
+
+namespace game_logic {}
+
+#endif /* !GAME_LOGIC_GAME_LOGIC_H_ */
