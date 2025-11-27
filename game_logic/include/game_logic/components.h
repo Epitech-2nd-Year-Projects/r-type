3ba7a3d15669a331361f9797d45fd3137ebe8ceb
Enzogall <enzo.gallini@epitech.eu>
2025-11-27 15:14:35 +0100

    fix (game_logic): Ai components

--- game_logic/include/game_logic/components.h
+++ game_logic/include/game_logic/components.h
@@ -1,6 +1,17 @@
 #ifndef GAME_LOGIC_COMPONENTS_H_
 #define GAME_LOGIC_COMPONENTS_H_
 
+#include "engine/ecs/component.h"
+#include "game_logic/components/ai_component.h"
+#include "game_logic/components/animation_component.h"
+#include "game_logic/components/damageable_component.h"
+#include "game_logic/components/health_component.h"
+#include "game_logic/components/player_component.h"
+#include "game_logic/components/powerup_component.h"
+#include "game_logic/components/score_value_component.h"
+#include "game_logic/components/sprite_component.h"
+#include "game_logic/components/weapon_component.h"
+
 namespace game_logic::components {}
 
 #endif /* !GAME_LOGIC_COMPONENTS_H_ */
