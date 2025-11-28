279c3fd927814af8112244915d1a2fbce7c7f0f2
LaurentAliu7 <laurent.aliu@epitech.eu>
2025-11-28 14:26:40 +0100

    feat(protocol): implement header encode and decode methode

--- protocol/src/header.cpp
+++ protocol/src/header.cpp
@@ -1,12 +1,44 @@
 #include "protocol/header.h"
-// #include "engine/net/buffer_reader.h"
 
-bool protocol::EncodeHeader(const Header& header, engine::net::BufferWriter& writer) {
-    // To be implemented after implementing BufferWriter methods.
-    return false;
+
+namespace protocol {
+
+bool EncodeHeader(const Header& header, engine::net::PacketBuffer& writer) {
+  writer.WriteUint16(header.version);
+  writer.WriteUint8(header.message_type);
+  writer.WriteUint8(header.flags);
+  writer.WriteUint32(header.sequence);
+  writer.WriteUint32(header.ack);
+  writer.WriteUint32(header.ack_bits);
+  writer.WriteUint32(header.timestamp_ms);
+  return true;
 }
 
-bool protocol::DecodeHeader(engine::net::BufferReader& reader, Header& out_header) {
-    // To be implemented after implementing BufferReader methods.
+bool DecodeHeader(engine::net::PacketBuffer& reader, Header& out_header) {
+  std::uint16_t version;
+  std::uint8_t message_type;
+  std::uint8_t flags;
+  std::uint32_t sequence;
+  std::uint32_t ack;
+  std::uint32_t ack_bits;
+  std::uint32_t timestamp_ms;
+
+  if (!reader.ReadUint16(version) ||
+      !reader.ReadUint8(message_type) ||
+      !reader.ReadUint8(flags) ||
+      !reader.ReadUint32(sequence) ||
+      !reader.ReadUint32(ack) ||
+      !reader.ReadUint32(ack_bits) ||
+      !reader.ReadUint32(timestamp_ms)) {
     return false;
+  }
+  out_header.version = version;
+  out_header.message_type = message_type;
+  out_header.flags = flags;
+  out_header.sequence = sequence;
+  out_header.ack = ack;
+  out_header.ack_bits = ack_bits;
+  out_header.timestamp_ms = timestamp_ms;
+  return true;
+}
 }
