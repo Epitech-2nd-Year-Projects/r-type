04f6ab5b18748926e2990e5bc1696428ec476186
gregorsternat <gregor.sternat@epitech.eu>
2025-11-30 22:20:22 +0100

    chore(client): update dependencies and include directories in xmake.lua

--- client/xmake.lua
+++ client/xmake.lua
@@ -3,5 +3,6 @@ add_rules("mode.debug", "mode.release")
 target("client")
 set_kind("binary")
 add_files("src/*.cpp")
-add_deps("protocol", "game_logic", "engine")
-
+add_includedirs("$(projectdir)/engine/include", "$(projectdir)/protocol/include",
+                {public = true})
+add_deps("engine", "protocol", "game_logic")
