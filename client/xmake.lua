f6a9d51a216801c6f6076e790a43e2cddb6f3308
gregorsternat <gregor.sternat@epitech.eu>
2025-11-30 22:23:17 +0100

    chore(client): remove public include directories from xmake.lua

--- client/xmake.lua
+++ client/xmake.lua
@@ -3,6 +3,4 @@ add_rules("mode.debug", "mode.release")
 target("client")
 set_kind("binary")
 add_files("src/*.cpp")
-add_includedirs("$(projectdir)/engine/include", "$(projectdir)/protocol/include",
-                {public = true})
 add_deps("engine", "protocol", "game_logic")
