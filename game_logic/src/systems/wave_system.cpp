61f4ee13b427436f13058c6940d5e0ff2049f1dd
Gallini <enzo.gallini@epitech.eu>
2025-12-10 09:12:09 +0100

    fix (gamelogic): Add more ennemies in the first wave

--- game_logic/src/systems/wave_system.cpp
+++ game_logic/src/systems/wave_system.cpp
@@ -13,40 +13,55 @@ void WaveSystem::LoadLevel(int level_id) {
 
   if (current_level_ == 1) {
     pending_spawns_.push_back(
-        {2.0f, WaveEnemyType::kPataPata, {850.0f, 100.0f}});
+        {2.0f, entities::EnemyType::kScout, {850.0f, 100.0f}});
     pending_spawns_.push_back(
-        {3.0f, WaveEnemyType::kPataPata, {850.0f, 300.0f}});
+        {3.0f, entities::EnemyType::kScout, {850.0f, 300.0f}});
     pending_spawns_.push_back(
-        {4.0f, WaveEnemyType::kPataPata, {850.0f, 500.0f}});
-
-    pending_spawns_.push_back({8.0f, WaveEnemyType::kBydo, {900.0f, 200.0f}});
-    pending_spawns_.push_back({8.5f, WaveEnemyType::kBydo, {900.0f, 400.0f}});
-    pending_spawns_.push_back({9.0f, WaveEnemyType::kBydo, {900.0f, 200.0f}});
+        {4.0f, entities::EnemyType::kScout, {850.0f, 500.0f}});
 
     pending_spawns_.push_back(
-        {15.0f, WaveEnemyType::kPataPata, {900.0f, 100.0f}});
+        {8.0f, entities::EnemyType::kBomber, {900.0f, 200.0f}});
     pending_spawns_.push_back(
-        {15.0f, WaveEnemyType::kPataPata, {900.0f, 500.0f}});
+        {8.5f, entities::EnemyType::kBomber, {900.0f, 400.0f}});
+    pending_spawns_.push_back(
+        {9.0f, entities::EnemyType::kBomber, {900.0f, 200.0f}});
+
+    pending_spawns_.push_back(
+        {12.0f, entities::EnemyType::kInterceptor, {900.0f, 100.0f}});
+    pending_spawns_.push_back(
+        {12.5f, entities::EnemyType::kInterceptor, {900.0f, 300.0f}});
+    pending_spawns_.push_back(
+        {13.0f, entities::EnemyType::kInterceptor, {900.0f, 500.0f}});
+
+    pending_spawns_.push_back(
+        {15.0f, entities::EnemyType::kScout, {900.0f, 100.0f}});
+    pending_spawns_.push_back(
+        {15.0f, entities::EnemyType::kScout, {900.0f, 500.0f}});
+    pending_spawns_.push_back(
+        {16.0f, entities::EnemyType::kInterceptor, {900.0f, 300.0f}});
+
+    pending_spawns_.push_back(
+        {20.0f, entities::EnemyType::kBomber, {900.0f, 150.0f}});
+    pending_spawns_.push_back(
+        {20.0f, entities::EnemyType::kBomber, {900.0f, 450.0f}});
+    pending_spawns_.push_back(
+        {22.0f, entities::EnemyType::kInterceptor, {900.0f, 100.0f}});
+    pending_spawns_.push_back(
+        {22.0f, entities::EnemyType::kInterceptor, {900.0f, 500.0f}});
+    pending_spawns_.push_back(
+        {24.0f, entities::EnemyType::kInterceptor, {900.0f, 300.0f}});
   }
 }
 
-void WaveSystem::Update(engine::ecs::Registry& registry,
+void WaveSystem::Update(engine::ecs::Registry &registry,
                         engine::time::TimeDelta dt) {
   current_wave_time_ += dt.as_seconds();
 
   while (!pending_spawns_.empty()) {
-    const auto& next_spawn = pending_spawns_.front();
+    const auto &next_spawn = pending_spawns_.front();
     if (current_wave_time_ >= next_spawn.spawn_time) {
-      switch (next_spawn.type) {
-        case WaveEnemyType::kPataPata:
-          game_logic::entities::EnemyBuilder::CreatePataPata(
-              registry, next_spawn.position);
-          break;
-        case WaveEnemyType::kBydo:
-          game_logic::entities::EnemyBuilder::CreateBydo(registry,
-                                                         next_spawn.position);
-          break;
-      }
+      game_logic::entities::EnemyBuilder::Create(registry, next_spawn.type,
+                                                 next_spawn.position);
       pending_spawns_.pop_front();
     } else {
       break;
