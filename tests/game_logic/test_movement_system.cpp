aeb9168dcfa2cbf8e0b3bfd0ab9623bab549e59a
dylan <dylan.ta@epitech.eu>
2025-12-10 19:42:28 +0100

    feat(test): add no-movement case when velocity is missing

--- tests/game_logic/test_movement_system.cpp
+++ tests/game_logic/test_movement_system.cpp
@@ -38,7 +38,7 @@ TEST(MovementSystemTest, UpdatesPositionBasedOnVelocityHalfSecond) {
 
     engine::ecs::EntityId entity = registry.SpawnEntity();
     registry.EmplaceComponent<engine::ecs::PositionComponent>(entity, 10.0f, 10.0f);
-    registry.EmplaceComponent<engine::ecs::VelocityComponent>(entity, 0.0f, 20.0f); // Moving up
+    registry.EmplaceComponent<engine::ecs::VelocityComponent>(entity, 0.0f, 20.0f);
 
     game_logic::systems::MovementSystem movementSystem;
     
@@ -51,3 +51,21 @@ TEST(MovementSystemTest, UpdatesPositionBasedOnVelocityHalfSecond) {
     EXPECT_FLOAT_EQ(pos->position.y, 20.0f);
 }
 
+TEST(MovementSystemTest, NoVelocityNoMovement) {
+    engine::ecs::Registry registry;
+    registry.RegisterComponent<engine::ecs::PositionComponent>();
+    registry.RegisterComponent<engine::ecs::VelocityComponent>();
+    registry.RegisterComponent<game_logic::components::PlayerComponent>();
+
+    engine::ecs::EntityId entity = registry.SpawnEntity();
+    registry.EmplaceComponent<engine::ecs::PositionComponent>(entity, 5.0f, 5.0f);
+
+    game_logic::systems::MovementSystem movementSystem;
+    engine::time::TimeDelta dt = engine::time::TimeDelta::from_seconds(1.0f);
+
+    movementSystem.Update(registry, dt);
+
+    auto& pos = registry.GetComponents<engine::ecs::PositionComponent>()[entity];
+    EXPECT_FLOAT_EQ(pos->position.x, 5.0f);
+    EXPECT_FLOAT_EQ(pos->position.y, 5.0f);
+}
