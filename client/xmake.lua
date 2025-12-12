b2b7ca1d6886b4f6e693a4a54ae41e5080cb12d1
gregorsternat <gregor.sternat@epitech.eu>
2025-12-12 14:08:42 +0100

    feat(xmake.lua): add nlohmann_json dependency for JSON handling in client project

--- client/xmake.lua
+++ client/xmake.lua
@@ -1,9 +1,11 @@
 add_rules("mode.debug", "mode.release")
 
+add_requires("nlohmann_json")
+
 target("client")
 set_kind("binary")
 add_files("src/**.cpp")
 add_includedirs("src")
 add_deps("protocol", "game_logic", "engine")
+add_packages("nlohmann_json")
 set_rundir("$(projectdir)")
-
