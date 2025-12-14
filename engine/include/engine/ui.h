524d57cc97408552ea8adc48570e92fb40cf1ba7
gregorsternat <gregor.sternat@epitech.eu>
2025-12-14 15:24:12 +0100

    chore(engine): add ui aggregate header

---
+++ engine/include/engine/ui.h
@@ -0,0 +1,10 @@
+#ifndef ENGINE_UI_H_
+#define ENGINE_UI_H_
+
+#include "engine/ui/canvas.h"
+#include "engine/ui/element.h"
+#include "engine/ui/layouts.h"
+#include "engine/ui/text.h"
+#include "engine/ui/types.h"
+
+#endif  // ENGINE_UI_H_
