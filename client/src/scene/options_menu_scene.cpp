2f7fc8a3caffd60afbf776fdb036c03e8a190d3e
gregorsternat <gregor.sternat@epitech.eu>
2025-12-24 15:05:31 +0100

    fix(options_menu_scene.cpp): update include path for engine_runtime.h to reflect new directory structure

--- client/src/scene/options_menu_scene.cpp
+++ client/src/scene/options_menu_scene.cpp
@@ -6,7 +6,7 @@
 
 #include "application.h"
 #include "audio_paths.h"
-#include "engine/core/engine_runtime.h"
+#include "engine/app/engine_runtime.h"
 #include "engine/math/rect.h"
 
 namespace client {
