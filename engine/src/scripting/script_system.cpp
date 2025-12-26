65f85e060a73f5a630302a2bec17f1f34deca790
Enzogall <enzo.gallini@epitech.eu>
2025-12-26 11:29:08 +0100

    feat (Engine): Script sytem class implementation

---
+++ engine/src/scripting/script_system.cpp
@@ -0,0 +1,22 @@
+#include "engine/scripting/script_system.h"
+
+#include "engine/ecs/registry.h"
+#include "engine/util/logging.h"
+
+namespace engine::scripting {
+
+ScriptSystem::ScriptSystem(sol::function update_fn)
+    : update_fn_(std::move(update_fn)) {}
+
+void ScriptSystem::Update(ecs::Registry& registry, time::TimeDelta dt) {
+  if (!update_fn_.valid()) return;
+
+  auto result = update_fn_(dt.as_seconds(), std::ref(registry));
+
+  if (!result.valid()) {
+    sol::error err = result;
+    ENGINE_LOG_ERROR("Lua System Error: {}", err.what());
+  }
+}
+
+}  // namespace engine::scripting
