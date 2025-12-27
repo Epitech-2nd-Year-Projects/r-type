22bb047bc1b4d06c8471239fa5880cd2eefd906e
gregorsternat <gregor.sternat@epitech.eu>
2025-12-27 18:02:20 +0100

    fix(tests): update include path for input_buffer.h to reflect new directory structure

--- tests/client/test_input_buffer.cpp
+++ tests/client/test_input_buffer.cpp
@@ -1,7 +1,7 @@
 #include <gtest/gtest.h>
 #include <vector>
 
-#include "input_buffer.h"
+#include "input/input_buffer.h"
 
 TEST(InputBufferTest, CapturesPressAndReleaseInOrder) {
   client::InputBuffer buffer;
