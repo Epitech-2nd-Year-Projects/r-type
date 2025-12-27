7d2c1e61e3bf74a49e34c551781c84efa74d908e
gregorsternat <gregor.sternat@epitech.eu>
2025-12-27 20:34:39 +0100

    refactor(client): trim game over header

--- client/src/scene/game_over_scene.h
+++ client/src/scene/game_over_scene.h
@@ -17,7 +17,6 @@ class GameOverScene : public Scene {
   struct Stats {
     std::uint32_t score{0};
     std::uint32_t wave{1};
-    std::uint32_t level{1};
   };
 
   explicit GameOverScene(ClientContext& context, const Stats& stats);
@@ -34,10 +33,7 @@ class GameOverScene : public Scene {
   std::shared_ptr<engine::ui::TextElement> title_;
   std::shared_ptr<engine::ui::TextElement> score_text_;
   std::shared_ptr<engine::ui::TextElement> wave_level_text_;
-  
-  int selected_option_{0};
   std::shared_ptr<engine::ui::TextElement> menu_main_exit_;
-  std::shared_ptr<engine::ui::TextElement> menu_quit_;
 };
 
 }  // namespace client
