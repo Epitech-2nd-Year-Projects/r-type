68533d14be2f6e648eb936149fc4efb8b7082c72
Enzogall <enzo.gallini@epitech.eu>
2025-12-27 23:01:03 +0100

    chore(game_logic): remove obsolete enemy builder include

--- game_logic/include/game_logic/entities.h
+++ game_logic/include/game_logic/entities.h
@@ -1,7 +1,6 @@
 #ifndef GAME_LOGIC_ENTITIES_H_
 #define GAME_LOGIC_ENTITIES_H_
 
-#include "game_logic/entities/enemy_builder.h"
 #include "game_logic/entities/missile_builder.h"
 #include "game_logic/entities/missile_data.h"
 #include "game_logic/entities/obstacle_builder.h"
