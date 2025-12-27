3de1be05adb7701b3d57298cb8710a039662ba17
gregorsternat <gregor.sternat@epitech.eu>
2025-12-27 14:47:41 +0100

    refactor(menu):update main menu header

--- client/src/scene/main_menu_scene.h
+++ client/src/scene/main_menu_scene.h
@@ -1,14 +1,12 @@
 #ifndef CLIENT_SCENE_MAIN_MENU_SCENE_H_
 #define CLIENT_SCENE_MAIN_MENU_SCENE_H_
 
-#include <array>
 #include <memory>
-#include <string>
 #include <vector>
 
 #include "scene.h"
-#include "../ui/button.h"
-#include "../ui/ui_element.h"
+#include "ui/menu_effects.h"
+#include "engine/ui/button.h"
 #include "engine/ui/canvas.h"
 #include "engine/ui/layouts.h"
 #include "engine/ui/text.h"
@@ -25,27 +23,17 @@ class MainMenuScene : public Scene {
 
  private:
   void LayoutUi(engine::render::Renderer2D& renderer);
-  void DrawPointers(engine::render::Renderer2D& renderer);
   void DrawTitle(engine::render::Renderer2D& renderer);
 
   ClientContext& context_;
-  std::vector<std::shared_ptr<ui::UIElement>> ui_elements_;
-  std::shared_ptr<ui::Button> play_button_;
-  std::shared_ptr<ui::Button> settings_button_;
-  std::shared_ptr<ui::Button> quit_button_;
+  std::vector<std::shared_ptr<engine::ui::Button>> ui_elements_;
+  std::shared_ptr<engine::ui::Button> play_button_;
+  std::shared_ptr<engine::ui::Button> settings_button_;
+  std::shared_ptr<engine::ui::Button> quit_button_;
   engine::ui::Canvas canvas_;
   std::shared_ptr<engine::render::Texture2D> title_texture_;
   engine::math::RectF title_rect_{};
-  std::vector<std::shared_ptr<engine::render::Texture2D>> pointer_frames_;
-  std::string hover_sfx_path_;
-  std::string click_sfx_path_;
-  struct PointerState {
-    bool hovered{false};
-    bool was_hovered{false};
-    float elapsed{0.0f};
-    bool animating{false};
-  };
-  std::array<PointerState, 3> pointer_states_{};
+  ui::MenuEffects menu_effects_;
 };
 
 }  // namespace client
