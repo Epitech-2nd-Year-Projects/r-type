96c0a50194e732af806ee2488f6137b57d9f4261
gregorsternat <gregor.sternat@epitech.eu>
2025-12-27 16:51:04 +0100

    refactor(client): drop local type code

--- client/src/local_prediction.h
+++ client/src/local_prediction.h
@@ -71,7 +71,6 @@ class LocalPrediction {
   static constexpr float kReconciliationDuration = 0.1f;
   static constexpr float kWorldWidth = 1920.0f;
   static constexpr float kWorldHeight = 1080.0f;
-  static constexpr std::uint16_t kPlayerTypeCode = 1u;
 };
 
 }  // namespace client
