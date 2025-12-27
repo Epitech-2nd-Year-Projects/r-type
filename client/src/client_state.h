64d7f44004c6f074f22e3448814aa684d39d7985
gregorsternat <gregor.sternat@epitech.eu>
2025-12-27 03:41:03 +0100

    feat(client): add src/client_state.h

---
+++ client/src/client_state.h
@@ -0,0 +1,38 @@
+/**
+ * @file client_state
+ * @brief Client state types
+ */
+
+#ifndef CLIENT_CLIENT_STATE_H_
+#define CLIENT_CLIENT_STATE_H_
+
+#include <cstdint>
+
+namespace client {
+
+/**
+ * @brief High level client state controlling active screen
+ */
+enum class ClientState {
+  kMainMenu,
+  kLobby,
+  kSettings,
+  kConnecting,
+  kInGame,
+  kPaused,
+  kGameOver,
+  kDisconnected
+};
+
+/**
+ * @brief Snapshot of end of match stats
+ */
+struct GameOverStats {
+  std::uint32_t score{0};
+  std::uint32_t wave{1};
+  std::uint32_t level{1};
+};
+
+}  // namespace client
+
+#endif  // CLIENT_CLIENT_STATE_H_
