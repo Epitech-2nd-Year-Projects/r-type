8f166cc2eb1bd92896ea115a6db2b331a0bf416d
Enzogall <enzo.gallini@epitech.eu>
2025-12-28 11:30:17 +0100

    refactor(entities): remove obsolete builder includes

--- game_logic/include/game_logic/entities.h
+++ game_logic/include/game_logic/entities.h
@@ -1,11 +1,8 @@
 #ifndef GAME_LOGIC_ENTITIES_H_
 #define GAME_LOGIC_ENTITIES_H_
 
-#include "game_logic/entities/missile_builder.h"
-#include "game_logic/entities/missile_data.h"
 #include "game_logic/entities/obstacle_builder.h"
 #include "game_logic/entities/obstacle_data.h"
-#include "game_logic/entities/player_builder.h"
 
 namespace game_logic::entities {}
 
