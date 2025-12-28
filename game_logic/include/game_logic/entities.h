7897b558e8ade47310254153364f04ba38e9fd6c
Enzogall <enzo.gallini@epitech.eu>
2025-12-28 19:05:22 +0100

    refactor(game_logic): remove legacy builder includes

--- game_logic/include/game_logic/entities.h
+++ game_logic/include/game_logic/entities.h
@@ -1,9 +1,6 @@
 #ifndef GAME_LOGIC_ENTITIES_H_
 #define GAME_LOGIC_ENTITIES_H_
 
-#include "game_logic/entities/obstacle_builder.h"
-#include "game_logic/entities/obstacle_data.h"
-
 namespace game_logic::entities {}
 
 #endif  // GAME_LOGIC_ENTITIES_H_
