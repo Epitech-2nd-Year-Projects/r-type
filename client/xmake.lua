f4d5c7db8a44b76d2052d7d12b440d141b91a2cf
gregorsternat <gregor.sternat@epitech.eu>
2025-12-29 14:29:58 +0100

    build(client): Compile raylib-media source and link FFmpeg libraries for video playback

--- client/xmake.lua
+++ client/xmake.lua
@@ -1,11 +1,14 @@
 add_rules("mode.debug", "mode.release")
 
 add_requires("nlohmann_json")
+add_requires("raylib")
 
 target("client")
 set_kind("binary")
 add_files("src/**.cpp")
-add_includedirs("src")
+add_files("../third_party/raylib-media/src/rmedia.c")
+add_includedirs("src", "../third_party/raylib-media/src")
 add_deps("protocol", "game_logic", "engine")
-add_packages("nlohmann_json")
+add_packages("nlohmann_json", "raylib")
+add_syslinks("avcodec", "avformat", "avutil", "swresample", "swscale")
 set_rundir("$(projectdir)")
