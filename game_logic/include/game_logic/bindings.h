5b3c8f33ee33d5b03ff6f986457967e21d39ff5b
Enzogall <enzo.gallini@epitech.eu>
2025-12-29 16:43:28 +0100

    refactor(bindings): update BindRuntimeTypes signature

--- game_logic/include/game_logic/bindings.h
+++ game_logic/include/game_logic/bindings.h
@@ -3,13 +3,17 @@
 
 #include <sol/sol.hpp>
 
+#include "engine/scripting/prefab_factory.h"
+
 namespace game_logic {
 
 /**
  * @brief Bind game-specific runtime types to Lua (Components, Systems helpers)
  * @param lua The Lua state to bind to
+ * @param factory The PrefabFactory for spawning entities
  */
-void BindRuntimeTypes(sol::state& lua);
+void BindRuntimeTypes(sol::state& lua,
+                      engine::scripting::PrefabFactory& factory);
 
 }  // namespace game_logic
 
