680a56cc09cfd46a6dbc7057afcccee3fb9c2e96
Yanis <yanis.kernoua@epitech.eu>
2025-12-31 21:29:09 +0100

    feat(console): implement console module with command execution and logging capabilities

---
+++ engine/include/engine/console.h
@@ -0,0 +1,10 @@
+#ifndef ENGINE_CONSOLE_H_
+#define ENGINE_CONSOLE_H_
+
+#include "engine/console/command.h"
+#include "engine/console/console.h"
+#include "engine/console/console_overlay.h"
+#include "engine/console/cvar.h"
+
+#endif  // ENGINE_CONSOLE_H_
+
