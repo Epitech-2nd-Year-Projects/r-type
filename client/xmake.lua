46e687970bf9e98359a1116a3744023c5ad9c033
Yanis <yanis.kernoua@epitech.eu>
2026-01-01 00:48:16 +0100

    chore(dependencies): add ffmpeg as a required dependency with specific configurations

--- client/xmake.lua
+++ client/xmake.lua
@@ -2,22 +2,13 @@ add_rules("mode.debug", "mode.release")
 
 add_requires("nlohmann_json")
 add_requires("raylib")
+add_requires("ffmpeg", {configs = {gpl = false, avcodec = true, avformat = true, avutil = true, swresample = true, swscale = true}})
 
 target("client")
-set_kind("binary")
-add_files("src/**.cpp")
-add_files("../third_party/raylib-media/src/rmedia.c")
-add_includedirs("src", "../third_party/raylib-media/src")
-add_deps("protocol", "game_logic", "engine")
-add_packages("nlohmann_json", "raylib")
-if is_plat("windows") then
-    local ffmpeg_dir = os.getenv("FFMPEG_DIR")
-    if ffmpeg_dir then
-        add_includedirs(path.join(ffmpeg_dir, "include"))
-        add_linkdirs(path.join(ffmpeg_dir, "lib"))
-    end
-    add_links("avcodec", "avformat", "avutil", "swresample", "swscale")
-else
-    add_syslinks("avcodec", "avformat", "avutil", "swresample", "swscale")
-end
-set_rundir("$(projectdir)")
+    set_kind("binary")
+    add_files("src/**.cpp")
+    add_files("../third_party/raylib-media/src/rmedia.c")
+    add_includedirs("src", "../third_party/raylib-media/src")
+    add_deps("protocol", "game_logic", "engine")
+    add_packages("nlohmann_json", "raylib", "ffmpeg")
+    set_rundir("$(projectdir)")
