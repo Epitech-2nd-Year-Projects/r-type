90bddc96425a59b88af2a0373dc0ba5d56d5dd36
Yanis <yanis.kernoua@epitech.eu>
2026-01-01 01:01:24 +0100

    chore(dependencies): switch FFmpeg dependency to vcpkg with alias

--- client/xmake.lua
+++ client/xmake.lua
@@ -2,7 +2,7 @@ add_rules("mode.debug", "mode.release")
 
 add_requires("nlohmann_json")
 add_requires("raylib")
-add_requires("ffmpeg", {configs = {gpl = false, avdevice = false, avcodec = true, avformat = true, avutil = true, swresample = true, swscale = true}})
+add_requires("vcpkg::ffmpeg", {alias = "ffmpeg"})
 
 target("client")
     set_kind("binary")
