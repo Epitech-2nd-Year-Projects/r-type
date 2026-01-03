505788fccd859cac9670b661b6cd8d325c75bede
Enzogall <enzo.gallini@epitech.eu>
2026-01-03 11:32:33 +0100

    feat(client): add engine_debug dependency

--- client/xmake.lua
+++ client/xmake.lua
@@ -8,7 +8,7 @@ set_kind("binary")
 add_files("src/**.cpp")
 add_files("../third_party/raylib-media/src/rmedia.c")
 add_includedirs("src", "../third_party/raylib-media/src")
-add_deps("protocol", "game_logic", "engine")
+add_deps("protocol", "game_logic", "engine", "engine_debug")
 add_packages("nlohmann_json", "raylib")
 if is_plat("windows") then
     local ffmpeg_dir = os.getenv("FFMPEG_DIR")
