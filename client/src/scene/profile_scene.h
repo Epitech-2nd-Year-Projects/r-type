79684da707597626fc26be3a25bc261de77e9217
dylan <dylan.ta@epitech.eu>
2026-01-06 16:12:08 +0100

    refactor(client): add menu effects support to profile scene

--- client/src/scene/profile_scene.h
+++ client/src/scene/profile_scene.h
@@ -11,6 +11,7 @@
 #include "engine/ui/text_input.h"
 #include "engine/ui/widget.h"
 #include "ui/avatar_renderer.h"
+#include "ui/menu_effects.h"
 
 namespace client {
 
@@ -49,6 +50,7 @@ class ProfileScene : public Scene {
   std::shared_ptr<engine::ui::Button> back_button_;
 
   std::vector<std::shared_ptr<engine::ui::Widget>> ui_elements_;
+  std::vector<std::shared_ptr<engine::ui::Button>> buttons_;
   bool text_input_focused_{false};
 
   std::unique_ptr<ui::AvatarRenderer> avatar_renderer_;
@@ -56,6 +58,8 @@ class ProfileScene : public Scene {
   std::shared_ptr<engine::ui::Button> avatar_left_button_;
   std::shared_ptr<engine::ui::Button> avatar_right_button_;
   engine::math::Vector2f avatar_position_{0.0f, 0.0f};
+
+  ui::MenuEffects menu_effects_;
 };
 
 }  // namespace client
