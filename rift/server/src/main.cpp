a4c83f10cc8bd146d26d289298a456af9a79f19f
Yanis <yanis.kernoua@epitech.eu>
2026-01-10 19:56:34 +0100

    feat(rift): implement server initialization and main loop in main function

--- rift/server/src/main.cpp
+++ rift/server/src/main.cpp
@@ -1,6 +1,19 @@
-#include <iostream>
+#include "engine/util/logging.h"
+#include "server_config.h"
+#include "server_runtime.h"
 
-int main() {
-    std::cout << "Rift Server" << std::endl;
-    return 0;
+int main(int argc, char** argv) {
+  (void)argc;
+  (void)argv;
+
+  server::ServerConfig config = server::LoadServerConfig();
+  server::ServerRuntime runtime(config);
+  if (const auto start_error = runtime.Start(); start_error) {
+    engine::util::Logger::Default().Error("[rift_server] Failed to start server: ",
+                                          start_error.message());
+    return 1;
+  }
+
+  runtime.RunMainLoop();
+  return 0;
 }
