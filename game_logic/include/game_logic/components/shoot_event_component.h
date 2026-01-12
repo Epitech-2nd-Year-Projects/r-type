656e2f48a9e1491978b404da9c12065e06181906
LaurentAliu7 <laurent.aliu@epitech.eu>
2026-01-12 13:48:36 +0100

    style: modifies code to fit with clang format

--- game_logic/include/game_logic/components/shoot_event_component.h
+++ game_logic/include/game_logic/components/shoot_event_component.h
@@ -1,17 +1,20 @@
 #ifndef GAME_LOGIC_COMPONENTS_SHOOT_EVENT_COMPONENT_H_
 #define GAME_LOGIC_COMPONENTS_SHOOT_EVENT_COMPONENT_H_
 
-#include "engine/math/vector2.h"
 #include <cstdint>
+
+#include "engine/math/vector2.h"
 
 namespace game_logic::components {
 
 /**
- * @brief Component attached to a player to trigger a shoot event with lag compensation.
- * 
+ * @brief Component attached to a player to trigger a shoot event with lag
+ * compensation.
+ *
  * If 'fired' is true, the weapon system will spawn a projectile.
- * 'spawn_position' is where the player WAS when they pressed the button (server-rewound).
- * 'latency_s' is how much time has passed since the shot (to fast-forward the projectile).
+ * 'spawn_position' is where the player WAS when they pressed the button
+ * (server-rewound). 'latency_s' is how much time has passed since the shot (to
+ * fast-forward the projectile).
  */
 struct ShootEventComponent {
   bool fired{false};
