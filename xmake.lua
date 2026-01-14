f00abfb906c836fef157f0725f2835edad7ddac1
LaurentAliu7 <laurent.aliu@epitech.eu>
2026-01-14 17:54:41 +0100

    fix(build): force system dependencies for xorg/nasm and align versions

--- xmake.lua
+++ xmake.lua
@@ -2,13 +2,16 @@ set_project("r-type")
 set_version("0.1.0")
 set_xmakever("3.0.6")
 
-add_rules("mode.debug", "mode.release")
+add_rules("mode.debug",-- FORCE resolve conflict by aligning versions AND prefering system libs
+"mode.release")
 set_languages("cxx23")
 set_warnings("all")
 set_policy("package.requires_lock", true)
 
-add_requireconfs("**.libxrender", {override = true, version = "0.9.12"})
-add_requireconfs("**.libxext", {override = true, version = "1.3.6"})
+add_requireconfs("**.libxrender", {override = true, version = "0.9.12", configs = {system = true}})
+add_requireconfs("**.libxext", {override = true, version = "1.3.6", configs = {system = true}})
+-- Avoid building nasm from source if possible
+add_requireconfs("**.nasm", {override = true, configs = {system = true}})
 
 add_requires("lz4")
 
