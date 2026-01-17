0748573ae1d17d9a9260029947323ad7dd0e36cf
Yanis <yanis.kernoua@epitech.eu>
2026-01-17 19:09:54 +0100

    fix(ffmpeg-prebuilt): update URL and version hash for prebuilt binaries

--- xmake.lua
+++ xmake.lua
@@ -7,12 +7,15 @@ set_languages("cxx23")
 set_warnings("all")
 set_policy("package.requires_lock", false)
 
+local FFMPEG_AUTOBUILD_TAG = "autobuild-2026-01-17-13-00"
+local FFMPEG_BUILD_SUFFIX = "7.1.3-34-g09dc319bf3"
+
 package("ffmpeg-prebuilt")
     set_homepage("https://github.com/BtbN/FFmpeg-Builds")
     set_description("Prebuilt FFmpeg binaries (LGPL)")
     set_license("LGPL-2.1")
-    set_urls("https://github.com/BtbN/FFmpeg-Builds/releases/download/autobuild-2026-01-17-13-00/ffmpeg-n$(version)-latest-win64-lgpl-shared-$(version).zip")
-    add_versions("7.1", "b10df74658d3f57dfd24a6a9cb81d4b9c2b5b77ea53cda13ee692f0047b21945")
+    set_urls("https://github.com/BtbN/FFmpeg-Builds/releases/download/" .. FFMPEG_AUTOBUILD_TAG .. "/ffmpeg-n" .. FFMPEG_BUILD_SUFFIX .. "-win64-lgpl-shared-$(version).zip")
+    add_versions("7.1", "5d8d4378582bf75039e9d114d345ae73e44cd5fe674c9b0d52ccf05a8ad7c68b")
     on_load("windows", function (package)
         package:add("links", "avcodec", "avformat", "avutil", "swresample", "swscale")
         package:add("includedirs", "include")
@@ -21,7 +24,7 @@ package("ffmpeg-prebuilt")
     end)
     on_install("windows", function (package)
         local version = package:version_str()
-        local root = "ffmpeg-n" .. version .. "-latest-win64-lgpl-shared-" .. version
+        local root = "ffmpeg-n" .. FFMPEG_BUILD_SUFFIX .. "-win64-lgpl-shared-" .. version
         if os.isdir(root) then
             os.cd(root)
         end
