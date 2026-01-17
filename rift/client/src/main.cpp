2660b4503ee0ddf08efabfbb10170a9cddc9b632
Yanis <yanis.kernoua@epitech.eu>
2026-01-17 13:34:37 +0100

    feat(rift): implement main function to initialize Rift client and connect to server

--- rift/client/src/main.cpp
+++ rift/client/src/main.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
 
-int main() {
-    std::cout << "Rift Client" << std::endl;
-    return 0;
+#include "application.h"
+#include "rift_config.h"
+
+int main(int argc, char** argv) {
+  rift::client::RiftConfig config = rift::client::LoadRiftConfig(argc, argv);
+
+  std::cout << "Rift Client" << std::endl;
+  std::cout << "Connecting to " << config.host << ":" << config.port
+            << std::endl;
+
+  rift::client::Application app(config);
+  return app.Run();
 }
